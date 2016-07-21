b3dabba31d64f617328650277457306ebff27d55
Shuvro Guha <shuvro@Shuvros-MacBook-Pro.local>
2016-07-21 14:47:54 -0400

    Encode working (probably)

--- LSB-Steg.py
+++ LSB-Steg.py
@@ -7,22 +7,59 @@ from skimage import io, color, util
 print("Begin")
 
 # convert to 8 bit color
-cover = Image.open("cover.png").convert("P")
-hidden = Image.open("dollar.png").convert("P")
-print cover.palette
+cover = Image.open("cover2.png").convert("RGB")
+hidden = Image.open("dollar.png").convert("RGB")
+
 
 # TODO: for ease right now
 cover = cover.resize((100,100))
-#cover.show()
+cover.show()
+
+# split into 3 color bands
+cover_r, cover_g, cover_b = cover.split()
+hidden_r, hidden_g, hidden_b = hidden.split()
 
 # check that cover image is big enough
 if ((cover.width * cover.height) < (8 * (hidden.width * hidden.height))):
 	sys.exit("Cover image not large enough")
 
-cover_b = list(cover.getdata())
-hidden_b = list(hidden.getdata())
+cover_r_pix = list(cover_r.getdata())
+cover_g_pix = list(cover_g.getdata())
+cover_b_pix = list(cover_b.getdata())
+hidden_r_pix = list(hidden_r.getdata())
+hidden_g_pix = list(hidden_g.getdata())
+hidden_b_pix = list(hidden_b.getdata())
+
 bitmasks = [1, 2, 4, 8, 16, 32, 64, 128]
 
+i = 0
+for pixel in hidden_r_pix:
+	for mask in bitmasks:
+		if( (pixel & mask) == mask):
+			cover_r_pix[i] = cover_r_pix[i] | 1
+		else:
+			cover_r_pix[i] = cover_r_pix[i] & ~(1)
+		i = i + 1
+
+i = 0
+for pixel in hidden_g_pix:
+	for mask in bitmasks:
+		if( (pixel & mask) == mask):
+			cover_g_pix[i] = cover_g_pix[i] | 1
+		else:
+			cover_g_pix[i] = cover_g_pix[i] & ~(1)
+		i = i + 1
+
+i = 0
+for pixel in hidden_b_pix:
+	for mask in bitmasks:
+		if( (pixel & mask) == mask):
+			cover_b_pix[i] = cover_b_pix[i] | 1
+		else:
+			cover_b_pix[i] = cover_b_pix[i] & ~(1)
+		i = i + 1
+
+'''
 i = 0
 # get a pixel from hidden image
 for pixel in hidden_b:
@@ -32,26 +69,28 @@ for pixel in hidden_b:
 		# if its a 1, change cover pixel's LSB to 1
 		# otherwise change cover pixel's LSB to 0
 		if ((pixel & mask) == mask):
-			temp = cover_b[i]
 			cover_b[i] = cover_b[i] | 1
-			if (abs(temp - cover_b[i]) > 1):
-				print("big diff")
 		else:
-			temp = cover_b[i]
-			cover_b[i] = cover_b[i] >> 1
-			cover_b[i] = cover_b[i] << 1
-			if (abs(temp - cover_b[i]) > 1):
-				print("big diff")
-		i = i + 1
+			cover_b[i] = cover_b[i] & ~(1)
 
-finished = Image.new(cover.mode, cover.size)
-finished.putdata(cover_b, scale = 1.0, offset = 0.0)
-finished.palette = cover.palette
-finished.show()
+		i = i + 1
+'''
+combined = zip(cover_r_pix, cover_g_pix, cover_b_pix)
+final = Image.new(cover.mode, cover.size)
+final.putdata(combined)
+final.show()
+
+''' Decode '''
+'''
+j = 0
+decoded = []
+for pixel in cover_b:
+	if(j > 7):
+		j = 0
+
+'''
+
+
+
 		
 print("End")
-#im.show()
-#r.show()
-#g.show()
-#a.show()	
-#b.show()
