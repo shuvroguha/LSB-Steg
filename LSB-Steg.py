ed286b25a8d0c6456bb605022fec115a6af6b691
Shuvro Guha <shuvro@Shuvros-MacBook-Pro.local>
2016-07-21 15:58:12 -0400

    Split into functions

--- LSB-Steg.py
+++ LSB-Steg.py
@@ -4,13 +4,45 @@ import sys
 from PIL import Image
 from skimage import io, color, util
 
+# hides the data from pixels_to_hide in the LSBs of cover_pixels
+def encode(pixels_to_hide, cover_pixels):
+	# bitmasks with 1 in position 0-7
+	bitmasks = [1, 2, 4, 8, 16, 32, 64, 128]
+
+	i = 0
+	for pixel in pixels_to_hide:
+		# inner loop runs 8 times - once for each bit in the pixel to hide
+		for mask in bitmasks:
+			# if the pixel's bit is 1, put a 1 at cover pixel's LSB
+			if( (pixel & mask) == mask):
+				cover_pixels[i] = cover_pixels[i] | 1
+			# if the pixel's bit is 0, put a 0 at cover pixel's LSB
+			else:
+				cover_pixels[i] = cover_pixels[i] & ~(1)
+			i = i + 1
+
+def decode(cover_pixels):
+	hidden_pixels = []
+	i = 0
+	px = 0
+
+	for pixel in cover_pixels:
+		if(i > 7):
+			hidden_pixels.append(px)
+			px = 0
+			i = 0
+		if( (pixel & 1) == 1):
+			px = px | (1 << i)
+		i = i + 1
+
+	return hidden_pixels
+
 print("Begin")
 
 # convert to 8 bit color
 # TODO: take inputs
 cover = Image.open("cover2.png").convert("RGB")
-hidden = Image.open("dollar.png").convert("RGB")
-
+message = Image.open("dollar.png").convert("RGB")
 
 # TODO: for ease right now
 cover = cover.resize((100,100))
@@ -18,55 +50,30 @@ cover = cover.resize((100,100))
 
 # split into 3 color bands
 cover_r, cover_g, cover_b = cover.split()
-hidden_r, hidden_g, hidden_b = hidden.split()
+message_r, message_g, message_b = message.split()
 
 # check that cover image is big enough
-if ((cover.width * cover.height) < (8 * (hidden.width * hidden.height))):
+if ((cover.width * cover.height) < (8 * (message.width * message.height))):
 	sys.exit("Cover image not large enough")
 
 # convert images to pixel values
 cover_r_pix = list(cover_r.getdata())
 cover_g_pix = list(cover_g.getdata())
 cover_b_pix = list(cover_b.getdata())
-hidden_r_pix = list(hidden_r.getdata())
-hidden_g_pix = list(hidden_g.getdata())
-hidden_b_pix = list(hidden_b.getdata())
+message_r_pix = list(message_r.getdata())
+message_g_pix = list(message_g.getdata())
+message_b_pix = list(message_b.getdata())
 
-# bitmasks with 1 in position 0-7
-bitmasks = [1, 2, 4, 8, 16, 32, 64, 128]
+# encode message in cover
+encode(message_r_pix, cover_r_pix)
+encode(message_g_pix, cover_g_pix)
+encode(message_b_pix, cover_b_pix)
 
-i = 0
-for pixel in hidden_r_pix:
-	for mask in bitmasks:
-		if( (pixel & mask) == mask):
-			cover_r_pix[i] = cover_r_pix[i] | 1
-		else:
-			cover_r_pix[i] = cover_r_pix[i] & ~(1)
-		i = i + 1
-
-i = 0
-for pixel in hidden_g_pix:
-	for mask in bitmasks:
-		if( (pixel & mask) == mask):
-			cover_g_pix[i] = cover_g_pix[i] | 1
-		else:
-			cover_g_pix[i] = cover_g_pix[i] & ~(1)
-		i = i + 1
-
-i = 0
-for pixel in hidden_b_pix:
-	for mask in bitmasks:
-		if( (pixel & mask) == mask):
-			cover_b_pix[i] = cover_b_pix[i] | 1
-		else:
-			cover_b_pix[i] = cover_b_pix[i] & ~(1)
-		i = i + 1
-
-
+# combine rbg into image
 combined = zip(cover_r_pix, cover_g_pix, cover_b_pix)
 final = Image.new(cover.mode, cover.size)
 final.putdata(combined)
-#final.show()
+final.show()
 
 ''' Decode '''
 
@@ -81,43 +88,12 @@ cover_r_pix = cover_r.getdata()
 cover_g_pix = cover_g.getdata()
 cover_b_pix = cover_b.getdata()
 
-hidden_r_pix = []
-hidden_g_pix = []
-hidden_b_pix = []
+# decode cover image bands
+hidden_r_pix = decode(cover_r_pix)
+hidden_g_pix = decode(cover_g_pix)
+hidden_b_pix = decode(cover_b_pix)
 
-i = 0
-hidden_pix = 0
-for pixel in cover_r_pix:
-	if(i > 7):
-		hidden_r_pix.append(hidden_pix)
-		hidden_pix = 0
-		i = 0
-	if( (pixel & 1) == 1):
-		hidden_pix = hidden_pix | (1 << i)
-	i = i + 1
-
-i = 0
-hidden_pix = 0
-for pixel in cover_g_pix:
-	if(i > 7):
-		hidden_g_pix.append(hidden_pix)
-		hidden_pix = 0
-		i = 0
-	if( (pixel & 1) == 1):
-		hidden_pix = hidden_pix | (1 << i)
-	i = i + 1
-
-i = 0
-hidden_pix = 0
-for pixel in cover_b_pix:
-	if(i > 7):
-		hidden_b_pix.append(hidden_pix)
-		hidden_pix = 0
-		i = 0
-	if( (pixel & 1) == 1):
-		hidden_pix = hidden_pix | (1 << i)
-	i = i + 1
-
+# combine rgb into the hidden message
 combined = zip(hidden_r_pix, hidden_g_pix, hidden_b_pix)
 final_hidden = Image.new("RGB", (32, 100))
 final_hidden.putdata(combined)
