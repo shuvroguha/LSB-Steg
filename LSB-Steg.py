16c0df2eef0527b3e565c151e6eae7fcc63fb9f7
Shuvro Guha <shuvro@Shuvros-MacBook-Pro.local>
2016-07-27 10:57:37 -0400

    adding flask

--- LSB-Steg.py
+++ LSB-Steg.py
@@ -30,17 +30,21 @@ def decode(cover_pixels, size):
 	i = 0
 	px = 0
 	cover_pixels = itertools.islice(cover_pixels, 26, (26 + size))
+
+	# construct one pixel (px) from every 8 pixels in cover image
+	# by looking at the LSB of each of the 8 pixels
 	for pixel in cover_pixels:
+		# add the constructed pixel to hidden_pixels after 8 loops
 		if(i > 7):
 			hidden_pixels.append(px)
 			count += 1
 			px = 0
 			i = 0
+		# if LSB of cover pixel is 1, make the bit in px 1
+		# otherwise do nothing, since it's already 0
 		if( (pixel & 1) == 1):
 			px = px | (1 << i)
 		i = i + 1
-
-	print count	
 
 	return hidden_pixels
 
@@ -66,6 +70,8 @@ message_r, message_g, message_b = message.split()
 # check that cover image is big enough
 if ((cover.width * cover.height) < (9 * (message.width * message.height))):
 	sys.exit("Cover image not large enough")
+
+''' Encode '''
 
 # convert images to pixel values
 cover_r_pix = list(cover_r.getdata())
@@ -135,5 +141,5 @@ combined_hidden = zip(hidden_r_pix, hidden_g_pix, hidden_b_pix)
 final_hidden = Image.new("RGB", (hidden_width, hidden_height))
 final_hidden.putdata(combined_hidden)
 final_hidden.show()
-		
+
 print("End")
